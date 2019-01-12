bbcc171af7265e18ea1fbf6e8a9e0772edbc9c5b
= <=>
2019-01-12 13:59:02 -0200

    Filtering by javascript the elements

--- src/public/todo-static/js/todo.js
+++ src/public/todo-static/js/todo.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function btMenu() {
+const btMenu = () => {
 	let btElement = document.querySelector('#bt-menu');
 	if( btElement )
 		btElement.addEventListener("click", function () {
@@ -8,7 +8,7 @@ function btMenu() {
 			element.classList.toggle("open");
 		})
 }
-function btCloseAlert() {
+const btCloseAlert = () => {
 	let btElement = document.querySelector('.bt-close-alert');
 	if( btElement )
 		btElement.addEventListener("click", function (e) {
@@ -17,7 +17,37 @@ function btCloseAlert() {
 		})
 }
 
+const filterByInnerTextNotContain = (elementSearch, text) => {
+		let taskList = document.querySelectorAll(elementSearch)
+		var arrTaskList = Array.from(taskList)
+		return arrTaskList.filter(e => e.innerText.indexOf(text)==-1 );
+}
+
+const showElements = (elementSearch) => 
+	document.querySelectorAll(elementSearch).
+		forEach( e => e.classList.remove('hidden'));
+
+const hideArrayElements = (Array) => 
+	Array.forEach( e => e.classList.add('hidden'));
+
+const search = () => {
+	let btElement = document.querySelector('#search');
+	if( btElement )
+		btElement.addEventListener("keyup", function (e) {
+			let ElementsFiltered = filterByInnerTextNotContain('#tasksList .list-group-item', e.target.value);
+			console.log(ElementsFiltered)
+			showElements('#tasksList .list-group-item')
+			hideArrayElements(ElementsFiltered)
+			
+			//let element = e.target.parentElement.parentElement;
+			//element.classList.add("hidden");
+		})
+}
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
 	btMenu()
 	btCloseAlert()
+	search()
 })
+
